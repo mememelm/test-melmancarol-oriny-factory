715c4ed2a244400e311029cee7e20a6130c03f82
mememelm <carol.arivony@gmail.com>
2020-07-16 23:36:46 +0300

    home
    - avatar home
    - disable back

--- cv-melman/src/app/app.component.ts
+++ cv-melman/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocationStrategy } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cv-melman';
+
+  constructor(private locationStrategy: LocationStrategy){
+    this.locationStrategy.onPopState(() => {
+      history.pushState(null, null, window.location.href)
+    })
+  }
 }
--- cv-melman/src/app/application/home/home.component.ts
+++ cv-melman/src/app/application/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  personFullName = 'rabearivony carol'
+  personSpeciality = 'frontend développeur'
+  personExperiences = '+3 ans d\'expériences'
+  personState = 'Disponible de suite'
+
   constructor() { }
 
   ngOnInit(): void {
