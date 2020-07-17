793ff9eaac15dfcc5699a23ff66be805bbb6c56c
mememelm <carol.arivony@gmail.com>
2020-07-17 16:32:02 +0300

    Squelette affichage CV
    - affichage information global

--- cv-melman/src/app/application/cv/global-information/global-information.component.ts
+++ cv-melman/src/app/application/cv/global-information/global-information.component.ts
@@ -9,11 +9,20 @@ export class GlobalInformationComponent implements OnInit {
 
   cvTitle = 'Carol'
 
-  personFullName = 'rabearivony carol'
-  personSpeciality = 'développeur frontend'
-  personExperiences = '+3 ans d\'expériences'
-  personState = 'Disponible de suite'
-  
+  // information
+  personLastName = 'rabearivony'
+  personFirstName = 'carol'
+  personAge = '28'
+  personSkill = 'développeur frontend'
+  personTechnology = 'angular'
+  personCellular = '+261 34 45 720 72'
+  personEmail = 'carol.arivony@gmail.com'
+  personLinkedin = 'https://www.linkedin.com/in/melman-carol'
+  personAddress = 'ambohimiandra - antananarivo'
+  personCountry = 'madagascar'
+ 
+  panelOpenState = false
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ cv-melman/src/app/constant/class/person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    personAddress: string
+    personAge: number
+    personCountry: string
+    personEmail: string
+    personLastName: string
+    personFirstName: string
+    personSkill: string
+    personTechnology: string
+}
